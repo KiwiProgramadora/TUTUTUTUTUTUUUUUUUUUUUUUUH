057292c1d527b7874806b675969b3b0a8c6bea87
KiwiProgramadora <90006186+KiwiProgramadora@users.noreply.github.com>
2022-01-18 19:08:16 -0300

    Y
    Y

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    fill("white")
+    textSize(15)
+    text("U T N I F C O N ", 100,160)
+    fill("lightblue")
+    text("Dica: um conjunto de comandos que realiza uma tarefa específica ", 100, 180)
+    // COPIEI E COLEI DO GOOGGLE :)//
+    fill("white")
+    textSize(15)
+    text("A T D A E A B S  ", 100,260)
+    fill("lightblue")
+    text("Dica: um local para armazenamento de dados ", 100, 280)
+    
+
+}
